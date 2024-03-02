4f43d0e76cda1e59a312536efc2bd6f66ae84080
sushil wakekar <sushwakekar@gmail.com>
2024-03-02 23:02:01 +0530

    Updates

---
+++ precache-manifest.e9d2e2672effed4ed996317a97a8f9e6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3328eae8a680f84d6315461720370c1",
+    "url": "/Univercity-Conference-Page/index.html"
+  },
+  {
+    "revision": "958fb42d2685ce72a22c",
+    "url": "/Univercity-Conference-Page/static/css/2.a6a3f75a.chunk.css"
+  },
+  {
+    "revision": "cecf88cf467c1d8e970f",
+    "url": "/Univercity-Conference-Page/static/css/main.0f89631a.chunk.css"
+  },
+  {
+    "revision": "958fb42d2685ce72a22c",
+    "url": "/Univercity-Conference-Page/static/js/2.986155a6.chunk.js"
+  },
+  {
+    "revision": "0a0b602d25062375dacd",
+    "url": "/Univercity-Conference-Page/static/js/3.40a56a06.chunk.js"
+  },
+  {
+    "revision": "cecf88cf467c1d8e970f",
+    "url": "/Univercity-Conference-Page/static/js/main.fcf9400c.chunk.js"
+  },
+  {
+    "revision": "e0c2cbb018a5a993e75e",
+    "url": "/Univercity-Conference-Page/static/js/runtime~main.8a7cb006.js"
+  },
+  {
+    "revision": "c9db92276162dafa79bc511f78198219",
+    "url": "/Univercity-Conference-Page/static/media/img1.c9db9227.jpg"
+  },
+  {
+    "revision": "817b3df7dced2b36a58eb46d576fc958",
+    "url": "/Univercity-Conference-Page/static/media/img2.817b3df7.jpg"
+  },
+  {
+    "revision": "f21023b93e98d49e489952aa87ea2325",
+    "url": "/Univercity-Conference-Page/static/media/img3.f21023b9.jpg"
+  },
+  {
+    "revision": "f8fdc4853063119b71561644ce50b414",
+    "url": "/Univercity-Conference-Page/static/media/img4.f8fdc485.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Univercity-Conference-Page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Univercity-Conference-Page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Univercity-Conference-Page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Univercity-Conference-Page/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "23d916baca638864da895b84698785aa",
+    "url": "/Univercity-Conference-Page/static/media/university.23d916ba.jpg"
+  }
+]);
