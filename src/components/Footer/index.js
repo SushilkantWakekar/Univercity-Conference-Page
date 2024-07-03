d612295661285d9fd351c290e04cdfb72144b6dd
sushil wakekar <sushwakekar@gmail.com>
2024-07-03 16:50:50 +0530

    commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,6 @@ import './index.css';
 import { FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
-import { FaLocationDot } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -12,16 +11,7 @@ const Footer = () => {
       <FaXTwitter className='social-icons'/>
       <FaInstagramSquare className='social-icons' />
 
-      <div className='address-bar'>
-        <FaLocationDot className='social-icons'/>
-        <div>
-          <p className='name'>X-university</p>
-          <p>XYZ Road X-Country </p>
-          <p>Y-District </p>
-          <p>Pin : 000</p>
-        </div>
-        
-      </div>
+      
     </div>
   )
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import university from './university.jpg';
-import Typewriter from "typewriter-effect";
 import ReactSlick from '../ReactSlick'
 import './index.css';
 import Themes from '../Themes';
@@ -11,18 +10,6 @@ const Home = () => {
   return (
     <>
     <div className='home'>
-      <div className='typewriter-container multicolor-text'>
-            <Typewriter 
-                onInit={(typewriter) => {
-                    typewriter
-                        .typeString("X-University")
-                        .pauseFor(1000)
-                        .deleteAll()
-                        .typeString("Welcomes You to conference")
-                        .start();
-                }}
-            />
-        </div>
         <div className='university-logo'>
             <img src= {university} alt='university'  className='university-img'/>
         </div>   
